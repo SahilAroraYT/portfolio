3988b64feea18435234058e6b70ca8f2aa73b866
Sahil Arora <113161975+SahilAroraYT@users.noreply.github.com>
2026-09-12 19:53:28 +0530

    updated skills

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
     remotePatterns: [
       { protocol: "https", hostname: "img.icons8.com" },
       { protocol: "https", hostname: "cdn.jsdelivr.net" },
+      { protocol: "https", hostname: "cdn.simpleicons.org" },
     ],
   },
 };
